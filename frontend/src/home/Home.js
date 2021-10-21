a1e5f015eaf3eb64d0cb775dd670d9d97c0a8df5
Artem Kolinko <kalinart@gmail.com>
2021-10-21 15:07:04 +0300

    Add frontend folder

---
+++ frontend/src/home/Home.js
@@ -0,0 +1,15 @@
+// do not need it from React 17+
+// import React from 'react'
+import './home.css';
+import {AcUnit} from '@material-ui/icons';
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <h1>Netflix clone</h1>
+      <AcUnit />
+    </div>
+  );
+};
+
+export default Home;
